be01b500067ac041d5d4970badcc970811dc06bc
nurdemirbas <54847231+nurdemirbas@users.noreply.github.com>
2020-05-15 00:36:03 +0300

    Add files via upload

---
+++ exceptions/IncorrectTextException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class IncorrectTextException extends Exception {
+
+}
---
+++ exceptions/IndexOutOfCapacityException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class IndexOutOfCapacityException extends Exception{
+
+}
