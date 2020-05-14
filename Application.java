66a473721e943e3c416ea00e478e44c6394ebb4c
nurdemirbas <54847231+nurdemirbas@users.noreply.github.com>
2020-05-15 00:31:14 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,11 @@
+import screens.MainScreen;
+
+public class Application {
+	public static void main(String[] args) {
+		
+		MainScreen homepage = new MainScreen();
+		homepage.open();
+			
+		
+	}
+}
